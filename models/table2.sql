e857184bc4029765b145d4333ad6618b0e625d21
venkataprasadk-neenopal <139958421+venkataprasadk-neenopal@users.noreply.github.com>
2023-08-05 15:15:03 +0530

    Update table2.sql

--- models/table2.sql
+++ models/table2.sql
@@ -3,7 +3,7 @@
 {{
     config(materialized='view')
 }}
-select "Country","Industry",Sum("Numberofemployees") as sum_emp
+select "Country","Industry",Sum("Number of employees") as sum_emp
 from dbt_prod.data
 group by "Country","Industry"
 order by "Country"
